61e8a956e5286c3b71662eccdcd26962ad82c023
SudheerMM6 <gwaz2071@gmail.com>
2026-03-06 09:04:51 +0530

    Update authUser.js

--- backend/middlewares/authUser.js
+++ backend/middlewares/authUser.js
@@ -1,22 +1,25 @@
 import jwt from 'jsonwebtoken'
 
-
 // user authentication middleware
 const authUser = async (req,res,next)=>{
      try {
+        
         const {token} = req.headers
         if (!token) {
-            // ADD 'return' HERE
             return res.json({success:false,message:'Not authorized Login'})
         } 
 
         const token_decode = jwt.verify(token,process.env.JWT_SECRET)
+
         req.body.userId = token_decode.id
+         
         next()
             
      } catch (error) {
+        
         console.log(error);
-        return res.json({success:false,message:'Not authorized Login'}) // You have it here, good.
+        return res.json({success:false,message:'Not authorized Login'})
      }
 }
+
 export default authUser
